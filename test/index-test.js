d0b233ca55754ed20696cedff81fc5f587f7c626
Jim Rountree <jimvrountree@gmail.com>
2019-12-08 19:27:59 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,6 @@
 
-describe('shout(string)', function shout(string) {
-  return string.uppercase()
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)', function() {
+    it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
