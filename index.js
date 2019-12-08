c1944ded8736717b058cc3c39a626e239f88ae92
Jim Rountree <jimvrountree@gmail.com>
2019-12-08 21:01:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logshout(string) {
-  console.log(string.touppercase)
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
