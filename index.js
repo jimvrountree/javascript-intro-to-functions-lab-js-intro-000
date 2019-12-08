c822ffd20a29b44fce22a9902ad61097d0d3ce9c
Jim Rountree <jimvrountree@gmail.com>
2019-12-08 22:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase){
+  if (string.toLowerCase()){
     return "I can't hear you!"
   }
-  if (string.toUpperCase){
+  if (string.toUpperCase()){
     return "YES INDEED!"
   }
 }
